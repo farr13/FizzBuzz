751931e7143dd744b1ebfb6696d25fe0424c9a54
farr13 <farrakhan.abdoulaye@mail.utoronto.ca>
2023-09-16 01:37:35 -0400

    Added Created class Reduce Added Added psvm Added While loop and print statement to calculate number of operations to reduce 100 to 0

---
+++ src/Reduce.java
@@ -0,0 +1,15 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int total = 0;
+        while (n != 0) {
+            total += 1;
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+        }
+        System.out.println(total);
+    }
+}
